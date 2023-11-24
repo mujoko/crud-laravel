2beef5ede812aa8d5b0efde8a408d69d56464b54
Mujoko <mujoko.mujoko@gmail.com>
2023-11-24 10:04:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/test', function () {
+    return view('test');
+});
 Route::resource('mahasiswa', mahasiswaController::class);
